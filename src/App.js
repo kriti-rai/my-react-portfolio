fa30cdaddaa1a2d448870105b22310c977dbfcda
github username <github email address>
2020-01-17 11:17:40 -0800

    Install react-router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <h1> Kriti Rai </h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
