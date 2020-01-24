062bcd0afe988697fa9638e697400373f9a216b2
github username <github email address>
2020-01-24 00:07:33 -0800

    Remove contactForm for now

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,17 +1,12 @@
 import React from 'react';
-import ContactForm from './ContactForm';
 import WaveAnimation from './WaveAnimation';
 import ContactMedia from './ContactMedia';
 
 const Contact = () => {
   return (
     <div className='contact'>
-      <WaveAnimation />
       <div className='contact-content'>
         <div className='contact-header'>Get in touch.</div>
-        <div className='contact-form'>
-          <ContactForm />
-        </div>
         <div className='contact-media'>
           <ContactMedia />
         </div>
