9865d958caedd5aca8ae82bb7e2ef0207c090476
github username <github email address>
2020-01-30 00:28:32 -0800

    Remove SideBarMenu and add link icons below the blurb

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,19 +1,29 @@
 import React from 'react';
-import SideBarMenu from '.././menu/SideBarMenu';
+import { Link } from 'react-router-dom';
 
 
 const Home = () => {
   return (
     <div className='home-content'>
-      <div className='home-left-col'>
-        <div className='header'>
-        Hello.
+      <div className='profile-pic'><img src={require('./profile-pic.jpg')} alt-text="Profile picture"></img></div>
+        <div className='home-left-col'>
+          <div className='header'>
+          Hello.
+          </div>
+          <div className='blurb'>
+            My name is Kriti Rai. I am a Full Stack Web Developer based in San Francisco Bay Area.
+          </div>
+          <div className='home-icons-container'>
+            <span><Link to ='/'><span className="home-icons"><i className="fas fa-home"></i></span></Link></span>
+            <span><Link to ='/bio'><span className="home-icons"><i className="fas fa-id-card"></i></span></Link></span>
+            <span><a target='_blank' href='https://kriti-rai.github.io/' className="home-icons"><i class="fas fa-blog"></i></a></span>
+            <span><Link to ='/portfolio'><span className="home-icons"><i class="fas fa-pencil-ruler"></i></span></Link></span>
+            <span><a target='_blank' href='https://github.com/kriti-rai/' className="home-icons"><i className="fab fa-github"></i></a></span>
+            <span><a target='_blank' href='https://www.linkedin.com/in/kriti-rai/' className="home-icons"><i className="fab fa-linkedin-in"></i></a></span>
+            <span><a target='_blank' href='https://open.spotify.com/playlist/7IdmpH7SHXDHFeerFRgrX5' className="home-icons"><i className="fab fa-spotify"></i></a></span>
+            <span><Link to ='/contact'><span className="home-icons"><i class="fas fa-envelope"></i></span></Link></span>
+          </div>
         </div>
-        <div className='blurb'>
-          My name is Kriti Rai. I am a Full Stack Web Developer based in San Francisco Bay Area.
-        </div>
-      </div>
-      <SideBarMenu />
     </div>
   )
 };
