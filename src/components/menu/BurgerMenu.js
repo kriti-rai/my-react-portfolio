5c7241232950a303dd64e0b1f28af29bd17823b7
github username <github email address>
2020-01-30 21:55:52 -0800

    Convert to class and add state, onClick event-handlers

--- src/components/menu/BurgerMenu.js
+++ src/components/menu/BurgerMenu.js
@@ -1,21 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { reveal as Menu } from 'react-burger-menu';
 
-const BurgerMenu = () => {
-  return (
-    <div className='burger-menu'>
-      <Menu right pageWrapId={ "page-wrap" } outerContainerId={ "outer-container" }>
-          <Link to ='/'><span className="menu-item">Home</span></Link>
-          <Link to ='/bio'><span className="menu-item">Bio</span></Link>
-          <a target='_blank' href='https://kriti-rai.github.io/' className="menu-item">Blog</a>
-          <Link to ='/portfolio'><span className="menu-item">Portfolio</span></Link>
-          <a target='_blank' href='https://github.com/kriti-rai/' className="menu-item">GitHub</a>
-          <a target='_blank' href='https://www.linkedin.com/in/kriti-rai/' className="menu-item">Linkedin</a>
-          <Link to ='/contact'><span className="menu-item">Contact</span></Link>
-      </Menu>
-    </div>
-  )
+class BurgerMenu extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      menuOpen: false
+    }
+  }
+
+  handleStateChange = state => {
+    this.setState({menuOpen: state.isOpen})
+  }
+
+  closeMenu = () => {
+    this.setState({menuOpen: false})
+  }
+
+  render () {
+    return (
+      <div className='burger-menu'>
+        <Menu
+          right pageWrapId={ "page-wrap" }
+          outerContainerId={ "outer-container" }
+          isOpen={this.state.menuOpen}
+          onStateChange={(state) => this.handleStateChange(state)}
+          >
+            <Link to ='/'><span className="menu-item" onClick={() => this.closeMenu()}>Home</span></Link>
+            <Link to ='/bio'><span className="menu-item" onClick={() => this.closeMenu()}>Bio</span></Link>
+            <a target='_blank' href='https://kriti-rai.github.io/' className="menu-item" onClick={() => this.closeMenu()}>Blog</a>
+            <Link to ='/portfolio'><span className="menu-item" onClick={() => this.closeMenu()}>Portfolio</span></Link>
+            <a target='_blank' href='https://github.com/kriti-rai/' className="menu-item" onClick={() => this.closeMenu()}>GitHub</a>
+            <a target='_blank' href='https://www.linkedin.com/in/kriti-rai/' className="menu-item" onClick={() => this.closeMenu()}>Linkedin</a>
+            <Link to ='/contact'><span className="menu-item" onClick={() => this.closeMenu()}>Contact</span></Link>
+        </Menu>
+      </div>
+    )
+  };
 };
 
 export default BurgerMenu;
