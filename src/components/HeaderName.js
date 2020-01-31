da0e40a141b53a1b4dfdb98ab5d630e2824590c1
github username <github email address>
2020-01-30 23:45:32 -0800

    Create HeaderName component

---
+++ src/components/HeaderName.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const HeaderName = () => {
+  return (
+    <div>
+      <Link to ='/'>Kriti Rai</Link>
+    </div>
+  )
+};
+
+export default HeaderName;
