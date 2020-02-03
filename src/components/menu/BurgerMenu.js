a3478556aa9cade79dcbc4b17bb4a5bf9fb933aa
github username <github email address>
2020-02-02 22:57:46 -0800

    Rearrange items

--- src/components/menu/BurgerMenu.js
+++ src/components/menu/BurgerMenu.js
@@ -29,11 +29,11 @@ class BurgerMenu extends Component {
           >
             <Link to ='/'><span className="menu-item" onClick={() => this.closeMenu()}>Home</span></Link>
             <Link to ='/bio'><span className="menu-item" onClick={() => this.closeMenu()}>Bio</span></Link>
-            <a target='_blank' href='https://kriti-rai.github.io/' className="menu-item" onClick={() => this.closeMenu()}>Blog</a>
             <Link to ='/portfolio'><span className="menu-item" onClick={() => this.closeMenu()}>Portfolio</span></Link>
+            <a target='_blank' href='https://kriti-rai.github.io/' className="menu-item" onClick={() => this.closeMenu()}>Blog</a>
             <a target='_blank' href='https://github.com/kriti-rai/' className="menu-item" onClick={() => this.closeMenu()}>GitHub</a>
             <a target='_blank' href='https://www.linkedin.com/in/kriti-rai/' className="menu-item" onClick={() => this.closeMenu()}>Linkedin</a>
-            <Link to ='/contact'><span className="menu-item" onClick={() => this.closeMenu()}>Contact</span></Link>
+            <a href="mailto:kriti.rai@gmail.com" className="menu-item" onClick={() => this.closeMenu()}>Contact</a>
         </Menu>
       </div>
     )
