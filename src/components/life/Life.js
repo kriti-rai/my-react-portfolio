cb6bc44c44fad7f06ad9067aa16f17400c6d8f66
github username <github email address>
2020-02-04 00:25:15 -0800

    Add Life.js

---
+++ src/components/life/Life.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import HeaderName from '.././HeaderName';
+
+const Life = () => {
+  return (
+    <div className='life-container'>
+      <div className='header-name-red'><HeaderName /></div>
+        <div class="life-img-container">
+          <img class="life-img" src={require('./images/1.jpg')}></img>
+          <img class="life-img" src={require('./images/2.jpg')}></img>
+          <img class="life-img" src={require('./images/3.jpg')}></img>
+          <img class="life-img" src={require('./images/4.jpg')}></img>
+          <img class="life-img" src={require('./images/5c.jpg')}></img>
+          <img class="life-img" src={require('./images/6.jpg')}></img>
+          <img class="life-img" src={require('./images/7.jpg')}></img>
+          <img class="life-img" src={require('./images/11.jpg')}></img>
+          <img class="life-img" src={require('./images/9.jpg')}></img>
+          <img class="life-img" src={require('./images/10.jpg')}></img>
+          <img class="life-img" src={require('./images/12.jpg')}></img>
+        </div>
+      </div>
+
+  )
+};
+
+export default Life;
