c0d9f6a5de64528054bb5fcc976ef9125c9da951
github username <github email address>
2020-02-05 22:58:55 -0800

    Change className to sidebar-menu-item

--- src/components/menu/SidebarMenu.js
+++ src/components/menu/SidebarMenu.js
@@ -5,13 +5,13 @@ import { Link } from 'react-router-dom';
 const SidebarMenu = () => {
   return (
     <div className='sidebar-menu'>
-      <span><Link to ='/bio'><span className="home-icons"><i className="fas fa-id-card"></i></span></Link></span>
-      <span><Link to ='/portfolio'><span className="home-icons"><i class="fas fa-briefcase"></i></span></Link></span>
-      <span><a target='_blank' href='https://kriti-rai.github.io/' className="home-icons"><i class="fas fa-pen-fancy"></i></a></span>
-      <span><a target='_blank' href='https://github.com/kriti-rai/' className="home-icons"><i className="fab fa-github"></i></a></span>
-      <span><a target='_blank' href='https://www.linkedin.com/in/kriti-rai/' className="home-icons"><i className="fab fa-linkedin-in"></i></a></span>
-      <span><a href="mailto:kriti.rai@gmail.com"><span className="home-icons"><i class="fas fa-envelope"></i></span></a></span>
-      <span><Link to ='/life'><span className="home-icons"><i class="fas fa-clock"></i></span></Link></span>
+      <span><Link to ='/bio'><span className="sidebar-menu-item"><i className="fas fa-id-card"></i></span></Link></span>
+      <span><Link to ='/portfolio'><span className="sidebar-menu-item"><i class="fas fa-briefcase"></i></span></Link></span>
+      <span><a target='_blank' href='https://kriti-rai.github.io/' className="sidebar-menu-item"><i class="fas fa-pen-fancy"></i></a></span>
+      <span><a target='_blank' href='https://github.com/kriti-rai/' className="sidebar-menu-item"><i className="fab fa-github"></i></a></span>
+      <span><a target='_blank' href='https://www.linkedin.com/in/kriti-rai/' className="sidebar-menu-item"><i className="fab fa-linkedin-in"></i></a></span>
+      <span><a href="mailto:kriti.rai@gmail.com"><span className="sidebar-menu-item"><i class="fas fa-envelope"></i></span></a></span>
+      <span><Link to ='/life'><span className="sidebar-menu-item"><i class="fas fa-clock"></i></span></Link></span>
     </div>
   )
 };
