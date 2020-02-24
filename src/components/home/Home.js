c6ef6bcafb25b150a671f40b79e74b4330507bee
github username <github email address>
2020-02-24 14:13:46 -0800

    Update content"

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -9,21 +9,24 @@ const Home = () => {
     <div className='home'>
       <div className='header-name-red'><HeaderName /></div>
       <div className='home-content'>
-        <div className='profile-pic'><img src={require('./images/profile-pic.jpg')} alt-text="Profile picture"></img></div>
         <div className='home-left-col'>
-          <div className='header'>
-          Hello.
+        <div className='profile-pic'><img src={require('./images/profile-pic.jpg')} alt-text="Profile picture"></img></div>
+          <div className="intro">
+            <div className='header'>
+              Hello.
+            </div>
+            <div className='blurb'>
+              My name is Kriti Rai. I am a Full Stack Web Developer based in San Francisco Bay Area.
+              I code in JavaScript but Ruby is my first love. A quick learner and an excessive coffee-drinker.
+              <div className="divider-red"></div>
+            </div>
           </div>
-          <div className='blurb'>
-            My name is Kriti Rai. I am a Full Stack Web Developer based in San Francisco Bay Area.
-            I code in JavaScript but Ruby is my first love. A quick learner and an excessive coffee-drinker.
-          </div>
-          <div className="divider-red"></div>
         </div>
         <div className='home-right-col'>
           <SidebarMenu />
         </div>
       </div>
+      <div className='phone-footer-container'></div>
     </div>
   )
 };
