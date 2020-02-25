b8e35b11d283313aa94588c72aacfe0715c10abe
github username <github email address>
2020-02-24 23:23:38 -0800

    Capitalize menu labels

--- src/components/menu/SidebarMenu.js
+++ src/components/menu/SidebarMenu.js
@@ -4,15 +4,15 @@ import { Link } from 'react-router-dom';
 
 const SidebarMenu = () => {
   return (
-    <div className='sidebar-menu'>
-      <span><Link to ='/bio'><span className="sidebar-menu-item"><i className="fas fa-id-card"></i><span className='menu-label'>bio</span></span></Link></span><br />
-      <span><Link to ='/portfolio'><span className="sidebar-menu-item"><i className="fas fa-briefcase"></i><span className='menu-label'>portfolio</span></span></Link></span><br />
-      <span><a target='_blank' href='https://kriti-rai.github.io/' className="sidebar-menu-item"><i className="fas fa-pen-fancy"></i><span className='menu-label'>blog</span></a></span><br />
-      <span><a target='_blank' href='https://github.com/kriti-rai/' className="sidebar-menu-item"><i className="fab fa-github"></i><span className='menu-label'>github</span></a></span><br />
-      <span><a target='_blank' href='https://www.linkedin.com/in/kriti-rai/' className="sidebar-menu-item"><i className="fab fa-linkedin-in"></i><span className='menu-label'>linkedin</span></a></span><br />
-      <span><a href="mailto:kriti.rai@gmail.com"><span className="sidebar-menu-item"><i className="fas fa-envelope"></i><span className='menu-label'>contact</span></span></a></span><br />
-      <span><Link to ='/life'><span className="sidebar-menu-item"><i className="fas fa-clock"></i><span className='menu-label'>life</span></span></Link></span>
-    </div>
+    <ul className='sidebar-menu'>
+      <li><Link to ='/bio'><span className="sidebar-menu-item"><i className="fas fa-id-card"></i><span className='menu-label'>Bio</span></span></Link></li>
+      <li><Link to ='/portfolio'><span className="sidebar-menu-item"><i className="fas fa-briefcase"></i><span className='menu-label'>Portfolio</span></span></Link></li>
+      <li><a target='_blank' href='https://kriti-rai.github.io/' className="sidebar-menu-item"><i className="fas fa-pen-fancy"></i><span className='menu-label'>Blog</span></a></li>
+      <li><a target='_blank' href='https://github.com/kriti-rai/' className="sidebar-menu-item"><i className="fab fa-github"></i><span className='menu-label'>GitHub</span></a></li>
+      <li><a target='_blank' href='https://www.linkedin.com/in/kriti-rai/' className="sidebar-menu-item"><i className="fab fa-linkedin-in"></i><span className='menu-label'>Linkedin</span></a></li>
+      <li><a href="mailto:kriti.rai@gmail.com"><span className="sidebar-menu-item"><i className="fas fa-envelope"></i><span className='menu-label'>Contact</span></span></a></li>
+      <li><Link to ='/life'><span className="sidebar-menu-item"><i className="fas fa-clock"></i><span className='menu-label'>Life</span></span></Link></li>
+    </ul>
   )
 };
 
