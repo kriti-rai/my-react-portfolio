19c33b708e946187d96999a0a40dce6660110877
kriti-rai <kriti.rai@gmail.com>
2020-06-30 02:02:05 -0700

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-This is my Portfolio website built from scratch using React and can be viewed at [My Website](https://kriti-rai.github.io/kritirai/).
-
-![my-website-screenshot](https://i.imgur.com/oyesUXp.png)
---
+++ precache-manifest.505451f8f77eda229f21a633f2a2738a.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a4430d219ba8fa04987bcf1ada939da",
+    "url": "/kritirai/index.html"
+  },
+  {
+    "revision": "424f8af87bd4bf0d0dbe",
+    "url": "/kritirai/static/css/2.7df321cc.chunk.css"
+  },
+  {
+    "revision": "d5cb540d04499e9fb468",
+    "url": "/kritirai/static/css/main.faa44fc0.chunk.css"
+  },
+  {
+    "revision": "424f8af87bd4bf0d0dbe",
+    "url": "/kritirai/static/js/2.533e7b2d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/kritirai/static/js/2.533e7b2d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d5cb540d04499e9fb468",
+    "url": "/kritirai/static/js/main.13a69cd4.chunk.js"
+  },
+  {
+    "revision": "2e830b3454db2002c845",
+    "url": "/kritirai/static/js/runtime-main.c6aa5766.js"
+  },
+  {
+    "revision": "568447e5c5014f7754a859c126a0f852",
+    "url": "/kritirai/static/media/1.568447e5.jpg"
+  },
+  {
+    "revision": "2e232d8eca99fe702c91a874a2423ab0",
+    "url": "/kritirai/static/media/10.2e232d8e.jpg"
+  },
+  {
+    "revision": "ecc0ce6d57d5b44073c103cf3123a53f",
+    "url": "/kritirai/static/media/11.ecc0ce6d.jpg"
+  },
+  {
+    "revision": "18e2db8f58acecc9283ea4e5d759a64c",
+    "url": "/kritirai/static/media/12.18e2db8f.jpg"
+  },
+  {
+    "revision": "5b691c6bb112f0fa68cce62b20d57001",
+    "url": "/kritirai/static/media/2.5b691c6b.jpg"
+  },
+  {
+    "revision": "209d5b129f2a95a2ed2b4fa50630e0b8",
+    "url": "/kritirai/static/media/3.209d5b12.jpg"
+  },
+  {
+    "revision": "9a981b0c1a5018b1229d8015fa1ee2af",
+    "url": "/kritirai/static/media/4.9a981b0c.jpg"
+  },
+  {
+    "revision": "e048c6baadb77b929a8a887989289150",
+    "url": "/kritirai/static/media/5c.e048c6ba.jpg"
+  },
+  {
+    "revision": "09e9e210de498a68868eb5dfa603eb30",
+    "url": "/kritirai/static/media/6.09e9e210.jpg"
+  },
+  {
+    "revision": "d4354b6fd11a8498bb5d0077eea8064b",
+    "url": "/kritirai/static/media/7.d4354b6f.jpg"
+  },
+  {
+    "revision": "daa25bc1b22b84bc8139038f72d3f31a",
+    "url": "/kritirai/static/media/9.daa25bc1.jpg"
+  },
+  {
+    "revision": "d1177cf7ea596e2a503c0067d050e1cd",
+    "url": "/kritirai/static/media/add-a-business-page.d1177cf7.png"
+  },
+  {
+    "revision": "a01dc091357ad8b47944ad8f26511e49",
+    "url": "/kritirai/static/media/article-by-number.a01dc091.png"
+  },
+  {
+    "revision": "7dafd46cfcd82f85d6bf7379f86dee32",
+    "url": "/kritirai/static/media/board.7dafd46c.png"
+  },
+  {
+    "revision": "ca8ac574734267bf89ef81f17305d742",
+    "url": "/kritirai/static/media/cover.ca8ac574.jpg"
+  },
+  {
+    "revision": "75074cf2abd241a4ff95eb87ce6f7c7e",
+    "url": "/kritirai/static/media/create-board.75074cf2.png"
+  },
+  {
+    "revision": "6116fba7858c29f31ac2651c1e8fc02d",
+    "url": "/kritirai/static/media/developers-page.6116fba7.png"
+  },
+  {
+    "revision": "5dea509e81694b81d09e78d08de35a69",
+    "url": "/kritirai/static/media/explore-page.5dea509e.png"
+  },
+  {
+    "revision": "400394ed37d533d8d40cca0a31db6892",
+    "url": "/kritirai/static/media/feed.400394ed.png"
+  },
+  {
+    "revision": "efb173162c16c8a59990ff7b772a0d58",
+    "url": "/kritirai/static/media/hike.efb17316.png"
+  },
+  {
+    "revision": "4e6bcba9bbfbdc0ba6555c32ddf768ae",
+    "url": "/kritirai/static/media/home.4e6bcba9.png"
+  },
+  {
+    "revision": "4209a794876bc1bb00c02bb3b80461c7",
+    "url": "/kritirai/static/media/homepage.4209a794.png"
+  },
+  {
+    "revision": "752e6890a24304516dc54ba99afe640e",
+    "url": "/kritirai/static/media/homepage.752e6890.png"
+  },
+  {
+    "revision": "574b9705f7d194817b007acd3d2a34f6",
+    "url": "/kritirai/static/media/list.574b9705.png"
+  },
+  {
+    "revision": "36fbb553ff4ceb0509227fab04c7e37a",
+    "url": "/kritirai/static/media/login.36fbb553.png"
+  },
+  {
+    "revision": "5eef3ba63bd14767a4eb0894c8babd20",
+    "url": "/kritirai/static/media/my-boards.5eef3ba6.png"
+  },
+  {
+    "revision": "851d5ee9930ed7f828979fc7c6f7834f",
+    "url": "/kritirai/static/media/objective.851d5ee9.png"
+  },
+  {
+    "revision": "bfc23b0bedd8afe6e0cf190308f9716e",
+    "url": "/kritirai/static/media/profile-on-pc.bfc23b0b.png"
+  },
+  {
+    "revision": "a980964243a07098fce444f996f123dc",
+    "url": "/kritirai/static/media/profile-pic.a9809642.jpg"
+  },
+  {
+    "revision": "509efcc7d80d77a2dd33286c80b4ba15",
+    "url": "/kritirai/static/media/register-on-pc.509efcc7.png"
+  },
+  {
+    "revision": "45a044a5f7b45c23a16f19026d465c79",
+    "url": "/kritirai/static/media/results-on-pc.45a044a5.png"
+  },
+  {
+    "revision": "e3d83d00816ffc469ddb40b711cb2ef3",
+    "url": "/kritirai/static/media/results.e3d83d00.png"
+  },
+  {
+    "revision": "b82e036569b0a643349d654c40217f62",
+    "url": "/kritirai/static/media/user.b82e0365.png"
+  },
+  {
+    "revision": "0d1e678e882c80ab5f11c2038c15bbd0",
+    "url": "/kritirai/static/media/users.0d1e678e.png"
+  }
+]);
