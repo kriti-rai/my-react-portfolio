ed2e70876a46b2f05ece4bfa66a240b9fd53de97
kriti-rai <kriti.rai@gmail.com>
2020-06-29 23:17:11 -0700

    Add href to the event

--- src/components/portfolio/projects/shop-equal/AboutShopEqual.js
+++ src/components/portfolio/projects/shop-equal/AboutShopEqual.js
@@ -7,8 +7,8 @@ const AboutShopEqual = () => {
         <p> Keywords: Ruby on Rails, React, Swagger, React Hooks </p>
           <p>
             In June 2020, I teamed up with four other developers (Michael Diaz, Valentine Maillard, Kah Yap and Rashan Smith)
-            to build a fully functioning web application for Hack for Black Lives Matter hackathon organized by NYC coders.
-            Our app is called Shop Equal and it intends to empower the Black community by directing users to Black-owned businesses.
+            to build a fully functioning web application for <a href='https://www.meetup.com/nyc-coders/events/271029282/#:~:text=NYC%20Coders%20is%20hacking%20in,and%20raise%20awareness%20for%20inequality' target='blank' style={{textDecoration:'underline', color:'white'}}>Hack for Black Lives Matter</a> hackathon organized by NYC coders.
+            Our app is called Shop Equal, a centralized and open-source interface, that intends to empower the Black community by directing users to Black-owned businesses.
             Most of my contributions were on the front-end side which is bootstrapped using `create-react-app`.
             I am also helping the team with web-scraping to collect as much data as possible on black-owned businesses.
           </p>
