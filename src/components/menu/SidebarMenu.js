b52597c575ca4e9320458852f8008f5776d14269
kriti-rai <kriti.rai@gmail.com>
2020-10-05 23:38:17 -0700

    Remove pen icon

--- src/components/menu/SidebarMenu.js
+++ src/components/menu/SidebarMenu.js
@@ -7,7 +7,6 @@ const SidebarMenu = () => {
     <ul className='sidebar-menu'>
       <li><Link to ='/bio'><span className="sidebar-menu-item"><i className="fas fa-id-card"></i><span className='menu-label'>Bio</span></span></Link></li>
       <li><Link to ='/portfolio'><span className="sidebar-menu-item"><i className="fas fa-briefcase"></i><span className='menu-label'>Portfolio</span></span></Link></li>
-      <li><a target='_blank' href='https://kriti-rai.github.io/' className="sidebar-menu-item"><i className="fas fa-pen-fancy"></i><span className='menu-label'>Blog</span></a></li>
       <li><a target='_blank' href='https://github.com/kriti-rai/' className="sidebar-menu-item"><i className="fab fa-github"></i><span className='menu-label'>GitHub</span></a></li>
       <li><a target='_blank' href='https://www.linkedin.com/in/kriti-rai/' className="sidebar-menu-item"><i className="fab fa-linkedin-in"></i><span className='menu-label'>Linkedin</span></a></li>
       <li><a href="mailto:kriti.rai@gmail.com"><span className="sidebar-menu-item"><i className="fas fa-envelope"></i><span className='menu-label'>Contact</span></span></a></li>
